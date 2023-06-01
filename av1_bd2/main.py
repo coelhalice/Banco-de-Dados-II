9451753015731e6ef4b2b93d8b4b9904d052d259
coelhalice <103080149+coelhalice@users.noreply.github.com>
2023-05-31 21:01:11 -0300

    Delete av1_bd2 directory

--- av1_bd2/main.py
+++
@@ -1,9 +0,0 @@
-from database import Database
-from motoristaDAO import MotoristaDAO
-from cli import MotoristaCLI
-
-db = Database(database="av1_bd2", collection="Motoristas")
-motoristaDAO = MotoristaDAO(database=db)
-
-motoristaCLI = MotoristaCLI(motoristaDAO)
-motoristaCLI.run()
